6df361f6a9665c4faa18d650a13f88e4bcea228e
Hitesh <hitesh221294@gmail.com>
2020-06-14 15:18:49 +0530

    config changes

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,28 @@ RUN npm ci
 # Bundle app source
 COPY . .
 
-# Set Port and run server
+# Build the project
+RUN npm run build
+
+# Set environment variables
+ENV POSTGRES_USERNAME=administrator
+ENV POSTGRES_PASSWORD=admin123
+ENV POSTGRES_HOST=cdr.czhjnmvrd1fr.us-east-1.rds.amazonaws.com
+ENV POSTGRES_DB=postgres
+ENV AWS_BUCKET=udagram-972405523754-dev
+ENV AWS_REGION=us-east-1
+ENV AWS_PROFILE=default
+ENV JWT_SECRET=hello
+ENV URL=http://localhost:8100
+
+# Bind the port that the image will run on
 EXPOSE 8080
-CMD [ "npm", "run", "prod" ]
+
+# Define the Docker image's behavior at runtime
+
+CMD ["node", "./www/server.js"]
+
+
+# Set Port and run server
+#EXPOSE 8080
+#CMD [ "npm", "run", "prod" ]
--- src/server.ts
+++ src/server.ts
@@ -6,33 +6,11 @@ import {IndexRouter} from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
 import {config} from './config/config';
-import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
+import {V0_FEED_MODELS} from './controllers/v0/model.index';
 
 
 (async () => {
-  const REQUIRED_ENVIRONMENT_SETTINGS = [
-    {name:"POSTGRES_USERNAME" , message:"Usernam of Postgres DB"},
-    {name:"POSTGRES_PASSWORD" , message:"Password of Postgres DB"},
-    {name:"POSTGRES_DB" , message:"DB Name of Postgres DB"},
-    {name:"POSTGRES_HOST" , message:"Host of Postgres DB"},
-    {name:"AWS_REGION" , message:"Our AWS Region"},
-    {name:"AWS_PROFILE" , message:"The used AWS Profile"},
-    {name:"AWS_BUCKET" , message:"The S3 Bucket"},
-    {name:"URL" , message:"Local URL"},
-    {name:"JWT_SECRET" , message:"Secret for JWT"},
-    ]
-
-  for(var env of REQUIRED_ENVIRONMENT_SETTINGS) {
-    if (!process.env[env.name]) {
-      console.error(`Environment variable ${env.name} should be set: ${env.message}`);
-    } else {
-      // convenient for debug; however: this line exposes all environment variable values - including any secret values they may contain
-      console.log(`Environment variable ${env.name} is set to : ${process.env[env.name]}`);
-    }
-  }
-
   await sequelize.addModels(V0_FEED_MODELS);
-  await sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
 
   const app = express();
@@ -47,7 +25,7 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
       'X-Access-Token', 'Authorization',
     ],
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: "*",
+    origin: config.url,
   }));
 
   app.use('/api/v0/', IndexRouter);
